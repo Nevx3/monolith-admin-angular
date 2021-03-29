3e4faa1bad9cacd86932e906a55970fc3ec69378
Ben Ankit <ben.ankit@ithands.com>
2021-03-29 16:05:16 +0530

    public directory for login and register updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PublicModule } from './public/public.module';
 import { SecureModule } from './secure/secure.module';
 
 @NgModule({
@@ -13,6 +14,7 @@ import { SecureModule } from './secure/secure.module';
     BrowserModule,
     AppRoutingModule,
     SecureModule,
+    PublicModule
   ],
   providers: [],
   bootstrap: [AppComponent]
