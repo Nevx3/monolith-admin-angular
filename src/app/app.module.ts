ce5e1a599104b6f77a8fb42e1e93a826e2db18aa
Ben Ankit <ben.ankit@ithands.com>
2021-03-29 15:30:00 +0530

    created secure and public module' ;)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavComponent } from './nav/nav.component';
-import { MenuComponent } from './menu/menu.component';
+import { SecureModule } from './secure/secure.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent,
-    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SecureModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
