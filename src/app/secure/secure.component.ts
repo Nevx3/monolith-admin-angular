401c888150c7766dc3bd715f28ddd9a316adc907
Ben Ankit <ben.ankit@ithands.com>
2021-03-29 15:42:21 +0530

    refactoring folder and files put all dashboard related files into secure module and folder

--- src/app/secure/secure.component.ts
+++ src/app/secure/secure.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NavComponent } from './nav/nav.component';
+import { MenuComponent } from './menu/menu.component';
 @Component({
   selector: 'app-secure',
   templateUrl: './secure.component.html',
--- src/app/secure/secure.module.ts
+++ src/app/secure/secure.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SecureComponent } from './secure.component';
-import { NavComponent } from './../nav/nav.component';
-import { MenuComponent } from './../menu/menu.component';
+import { NavComponent } from './nav/nav.component';
+import { MenuComponent } from './menu/menu.component';
 
 
 @NgModule({
